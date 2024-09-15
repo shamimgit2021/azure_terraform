e02417af8393156832e42020764c67f50caa3d61
Mike <Mike@example.com>
2024-09-16 03:09:09 +0530

    updated client secret

--- provider.tf
+++ provider.tf
@@ -11,6 +11,6 @@ provider "azurerm" {
   subscription_id            = "4a25f603-dcab-4159-96fc-edb1938e019e"
   tenant_id                  = "407dfb04-b176-4113-8576-11ade6dcd85b"
   client_id                  = "8e7c6fc2-6b15-456c-a1c8-0bb82be72dec"
-  client_secret              = "CMp8Q~ZtCvWiMkdTaN~lrudKKm6VBLYOgnoiibQa"
+  client_secret              = "GIe8Q~by6q8nzv0OO4UMfbMdUBDm4PXNw.OI4b46"
   skip_provider_registration = true
 }
